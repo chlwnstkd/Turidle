1b8dd5f61cbc45ecec65cda2415a712287011408
poly <jun1234755@naver.com>
2024-06-07 00:06:55 +0900

    2024-06-07

--- src/main/java/kopo/poly/persistance/mapper/IChatMapper.java
+++ src/main/java/kopo/poly/persistance/mapper/IChatMapper.java
@@ -1,6 +1,7 @@
 package kopo.poly.persistance.mapper;
 
 import kopo.poly.dto.ChatDTO;
+import kopo.poly.dto.UserInfoDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface IChatMapper {
     int saveMessage(ChatDTO pDTO) throws Exception; // 채팅 정보 조회
 
     List<ChatDTO> getMessage(String roomName) throws Exception;
+
+    void deleteChat(UserInfoDTO pDTO) throws Exception; // 채팅 삭제
 }
--- src/main/java/kopo/poly/service/IChatService.java
+++ src/main/java/kopo/poly/service/IChatService.java
@@ -11,6 +11,8 @@ public interface IChatService {
     int saveMessage(ChatDTO pDTO) throws Exception;
     List<ChatDTO> getMessage(String roomName) throws Exception;
 
-    List<Map<String, Object>> getChatList() throws Exception;
+    List<Map<String, Object>> getChatList(UserInfoDTO pDTO) throws Exception;
+
+    int getChatCount() throws Exception;
     int deleteUser(String roomName, String userId) throws Exception;
 }
