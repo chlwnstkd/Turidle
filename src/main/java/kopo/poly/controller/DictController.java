9fe1ee4ff548ed9ec87d5a5d37cd4c62e57f7d75
poly <jun1234755@naver.com>
2024-06-28 08:57:27 +0900

    2024-06-28

--- src/main/java/kopo/poly/controller/DictController.java
+++ src/main/java/kopo/poly/controller/DictController.java
@@ -1,11 +1,7 @@
 package kopo.poly.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import kopo.poly.dto.CommentDTO;
 import kopo.poly.dto.DictDTO;
-import kopo.poly.dto.MsgDTO;
-import kopo.poly.dto.PostDTO;
 import kopo.poly.service.IDictService;
 import kopo.poly.util.CmmUtil;
 import kopo.poly.util.EncryptUtil;
@@ -17,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,26 +22,37 @@ public class DictController {
 
     private final IDictService dictService;
 
+    /**
+     * 사전 검색 페이지로 이동
+     * @return 사전 검색 페이지 경로
+     */
     @GetMapping(value = "/dictSearch")
     public String search() {
-
         log.info(this.getClass().getName() + ".search Start!");
-
         return "dict/dictSearch";
     }
 
+    /**
+     * 사전 목록 조회
+     * @param model 모델 객체
+     * @param page 현재 페이지 번호 (기본값: 1)
+     * @param request HTTP 요청 객체
+     * @return 사전 목록 페이지 경로
+     * @throws Exception 예외 처리
+     */
     @GetMapping("/dictList")
     public String dictList(ModelMap model, @RequestParam(defaultValue = "1") int page, HttpServletRequest request)
             throws Exception {
         log.info(this.getClass().getName() + ".dictList Start!");
 
+        // 검색어를 요청 파라미터에서 가져와 복호화
         String text = CmmUtil.nvl(request.getParameter("text"));
-
         text = EncryptUtil.decodeString(text);
 
         log.info(text);
 
         try {
+            // 사전 목록 조회
             List<DictDTO> rList = dictService.getDictList(text);
             if (rList == null) rList = new ArrayList<>();
 
@@ -71,6 +77,7 @@ public class DictController {
 
             rList = rList.subList(fromIndex, toIndex);
 
+            // 모델에 필요한 데이터 추가
             model.addAttribute("rList", rList);
             model.addAttribute("currentPage", page);
             model.addAttribute("totalPages", totalPages);
@@ -81,32 +88,38 @@ public class DictController {
         }
 
         log.info(this.getClass().getName() + ".페이지 번호 : " + page);
-
         log.info(this.getClass().getName() + ".dictList End!");
 
         return "dict/dictList";
     }
+
+    /**
+     * 사전 상세 정보 조회
+     * @param model 모델 객체
+     * @param request HTTP 요청 객체
+     * @return 사전 상세 정보 페이지 경로
+     * @throws Exception 예외 처리
+     */
     @GetMapping("/dictInfo")
     public String dictInfo(ModelMap model, HttpServletRequest request)
             throws Exception {
         log.info(this.getClass().getName() + ".dictInfo Start!");
 
+        // 요청 파라미터에서 targetCode와 단어를 가져와 복호화
         String targetCode = CmmUtil.nvl(request.getParameter("targetCode"));
         String word = CmmUtil.nvl(request.getParameter("word"));
-
-
         word = EncryptUtil.decodeString(word);
 
         log.info(word);
-
         log.info("targetCode : " + targetCode);
 
+        // 사전 상세 정보 조회
         DictDTO rDTO = dictService.getDictInfo(targetCode);
-
         rDTO = rDTO.toBuilder().word(word).build();
 
         log.info(rDTO.toString());
 
+        // 모델에 사전 상세 정보 추가
         model.addAttribute("rDTO", rDTO);
 
         log.info(this.getClass().getName() + ".dictList End!");
