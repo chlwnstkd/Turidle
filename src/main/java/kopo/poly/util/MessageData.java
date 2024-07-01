4f315f1101288cb2dd8a2ab222ffe5da8abec7ec
poly <jun1234755@naver.com>
2024-07-02 05:32:59 +0900

    2024-07-02

---
+++ src/main/java/kopo/poly/util/MessageData.java
@@ -0,0 +1,14 @@
+package kopo.poly.util;
+
+public class MessageData {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
