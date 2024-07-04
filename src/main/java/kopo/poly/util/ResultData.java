637775739443ea18d28a9d68bc0c42480edda425
poly <jun1234755@naver.com>
2024-07-05 00:02:06 +0900

    2024-07-05

---
+++ src/main/java/kopo/poly/util/ResultData.java
@@ -0,0 +1,14 @@
+package kopo.poly.util;
+
+public class ResultData {
+    private MessageData message;
+
+    public MessageData getMessage() {
+        return message;
+    }
+
+    public void setMessage(MessageData message) {
+        this.message = message;
+    }
+
+}
