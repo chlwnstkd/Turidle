50c60ad03d3723677d6501cdfbf60f4b44f02cbc
poly <jun1234755@naver.com>
2024-07-19 10:24:01 +0900

    2024-07-19

--- src/main/java/kopo/poly/controller/TransController.java
+++ src/main/java/kopo/poly/controller/TransController.java
@@ -1,5 +1,6 @@
 package kopo.poly.controller;
 
+import kopo.poly.service.ISpeechService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,12 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TransController {
 
+    // SpeechService 주입
+    private ISpeechService speechService;
+
     @GetMapping(value = "")
     public String trans() {
 
         log.info(this.getClass().getName() + ".trans Start!");
 
         return "trans/trans";
+    }
+
+
+    @GetMapping("/recognize") // HTTP GET 엔드포인트
+    public String recognizeSpeech() throws Exception{
+        // 음성 인식을 수행하고 결과를 반환
+        return speechService.recognizeSpeech();
     }
 
 }
