56787b91b1bec8a69b30ffd9c369bcacee1fd124
poly <jun1234755@naver.com>
2024-10-02 00:00:21 +0900

    2024-10-02

--- src/main/java/kopo/poly/service/ISpeechService.java
+++ src/main/java/kopo/poly/service/ISpeechService.java
@@ -1,6 +1,11 @@
 package kopo.poly.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface ISpeechService {
 
     String streamingMicRecognize() throws Exception;
+
 }
