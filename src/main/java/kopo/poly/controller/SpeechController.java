a06d2a31aceeffb9c0349f22b5d6cd8e639162e6
poly <jun1234755@naver.com>
2024-10-21 09:09:57 +0900

    2024-10-21

--- src/main/java/kopo/poly/controller/SpeechController.java
+++ src/main/java/kopo/poly/controller/SpeechController.java
@@ -32,31 +32,9 @@ public class SpeechController {
     private final ISpeechService speechService;
 
 
-    @PostMapping("/stt")
-    public String startRecognition(@RequestBody Map<String, Object> audioFormat) {
+    @GetMapping("/stt")
+    public void startRecognition() {
+        speechService.testTranscribeWavFile();
 
-
-        System.out.println("샘플 레이트: " + audioFormat.get("sampleRate"));
-        System.out.println("샘플 비트 크기: " + audioFormat.get("sampleSizeInBits"));
-        System.out.println("채널 수: " + audioFormat.get("channels"));
-        System.out.println("signed: " + audioFormat.get("signed"));
-        System.out.println("bigEndian: " + audioFormat.get("bigEndian"));
-
-        // 요청에서 각각의 파라미터를 가져오기
-        int sampleRate = (Integer) audioFormat.get("sampleRate");
-        int sampleSizeInBits = (Integer) audioFormat.get("sampleSizeInBits");
-        int channels = (Integer) audioFormat.get("channels");
-        boolean signed = (Boolean) audioFormat.get("signed");
-        boolean bigEndian = (Boolean) audioFormat.get("bigEndian");
-
-        String[] format = {String.valueOf(sampleRate), String.valueOf(sampleSizeInBits),
-                String.valueOf(channels), String.valueOf(signed),String.valueOf(bigEndian)};
-
-        try {
-            return speechService.streamingMicRecognize(format);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error starting speech recognition: " + e.getMessage();
-        }
     }
 }
